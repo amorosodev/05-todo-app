651e871ac9035e62e27219535170ce06e5a9a7e5
amoroso <adames230501@gmail.com>
2024-11-29 06:41:26 -0500

    clase-store

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import { App } from './src/todo/app';
+
+
+console.log ('hola mundo ');
+
+
+
+App ('#app');
+
+
---
+++ src/todo/models/todo.js
@@ -0,0 +1,16 @@
+
+
+export class todo {
+
+    constructor( descrption ){
+        this.id = 1;
+        this.descrption = descrption;
+        this.done = false;
+        this.createdAt = new Date();
+
+    }
+
+
+}
+
+
